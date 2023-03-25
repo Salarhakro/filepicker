fdc82ff8cb5ca610c1a7add0f8a303fb5833328f
salarhakro <shahback321@gmail.com>
2023-03-25 20:10:43 +0500

    widget adde

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class _MyAppState extends State<MyApp> {
           children: [
             Text('hello'),
             Text('hello'),
+            Text('hello'),
           ],
         ),
       ),
