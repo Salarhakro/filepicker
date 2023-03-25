0a073d9be5a0f53a34a789955a7b3fe9dbff6627
salarhakro <shahback321@gmail.com>
2023-03-25 19:56:12 +0500

    filepicker

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('File Picker'),
+        ),
+        
+      ),
+    );
+  }
+}
